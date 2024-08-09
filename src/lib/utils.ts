0e14f65801a434da96deeaf38e4ab0aee2ecfd61
Rick Zhang <rick@thenarrative.dev>
2024-08-09 21:20:28 +1000

    Add functions to check for wins

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { CellValue } from "../components/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -32,4 +33,58 @@ export function determineBorders(
   }
 
   return borderClasses.join(" ");
+}
+
+export function checkLine(line: CellValue[]): CellValue {
+  if (line.every((value) => value === CellValue.X)) {
+    return CellValue.X;
+  }
+
+  if (line.every((value) => value === CellValue.O)) {
+    return CellValue.O;
+  }
+
+  return CellValue.EMPTY;
+}
+
+export function checkWinner(board: CellValue[][]): CellValue {
+  // Real quickness is to check cell by cell so you can short circuit the every logic
+  // Syntactically beautiful way would be to do it line by line though.
+  let winner = CellValue.EMPTY;
+
+  for (let i = 0; i < board.length; i++) {
+    const row = board[i];
+    winner = checkLine(row);
+
+    if (winner !== CellValue.EMPTY) {
+      return winner;
+    }
+
+    const column = board.map((row) => row[i]);
+    winner = checkLine(column);
+
+    if (winner !== CellValue.EMPTY) {
+      return winner;
+    }
+  }
+
+  const diagonal1 = [];
+  const diagonal2 = [];
+
+  for (let d = 0; d < board.length; d++) {
+    diagonal1.push(board[d][d]);
+    diagonal2.push(board[board.length - 1 - d][d]);
+  }
+
+  winner = checkLine(diagonal1);
+  if (winner !== CellValue.EMPTY) {
+    return winner;
+  }
+
+  winner = checkLine(diagonal2);
+  if (winner !== CellValue.EMPTY) {
+    return winner;
+  }
+
+  return winner;
 }
