0ed707232b5035cf266d21885bb4a8af015d2d08
Rick Zhang <rick@thenarrative.dev>
2024-08-09 16:35:45 +1000

    Add cursor pointer to reset button

--- src/components/ResetButton.tsx
+++ src/components/ResetButton.tsx
@@ -11,7 +11,7 @@ export const ResetButton: FC<Props> = ({ onReset }) => {
     <Flexbox justify="end">
       <RotateCcw
         onClick={onReset}
-        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 cursor-pointer"
       />
     </Flexbox>
   );
