15789c488b5aaacea9c348dede02585fca2968c7
Rick Zhang <rick@thenarrative.dev>
2024-08-17 21:56:54 +1200

    More references

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -1,6 +1,6 @@
 import { Moon, Sun } from "lucide-react";
 
-import { useTheme } from "./ThemeProvider";
+import { useTheme } from "./providers/ThemeProvider";
 import {
   DropdownMenu,
   DropdownMenuContent,
